a6ca815157143e484c29916cefe02d5680afdd1d
Robertl231 <rloughran67@gmail.com>
2021-12-03 09:30:22 +0000

    more deployment steps taken

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn game_hut.wsgi:application
--- game_hut/settings.py
+++ game_hut/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&de&!$q8x-iierj037k^%h+%e3@v4nrj6f6_)hrpcf$k&6&168
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['game-hut.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -109,12 +109,17 @@ WSGI_APPLICATION = 'game_hut.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
